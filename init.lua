03c66f5b0f3984d4e3d57b0d7e182a0293c1e6c0
John Vivian <jtvivian@gmail.com>
2024-03-11 14:32:24 -0700

    Add nvim tree

--- init.lua
+++ init.lua
@@ -800,6 +800,18 @@ require('lazy').setup({
     end,
   },
 
+  {
+    "nvim-tree/nvim-tree.lua",
+    version = "*",
+    lazy = false,
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      require("nvim-tree").setup {}
+    end,
+  }
+
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- put them in the right spots if you want.
